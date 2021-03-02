f3183756a3354f63b00eccf42a31140a0cba7206
maks228998 <78788414+maks228998@users.noreply.github.com>
2021-03-02 12:02:57 +0600

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<cstdlib>
+#include "my_lib.h"
+
+int main() {
+	printf("Find the smallest element of the generated array\n");
+
+	srand(time(NULL));
+
+	printf("Enter the minimum element of array:\n");
+	const double min = input_double_number();
+
+	printf("Enter the maximum element of array:\n");
+	const double max = input_double_number();
+
+	printf("Generated element of array:\n");
+
+	const int arrays_lenght = 32;
+	double array[arrays_lenght];
+	double min_array_element = max + 1.0;
+
+	for (int counter = 0; counter < arrays_lenght; counter++) {
+		array[counter] = double_rand_min_max(min, max);
+		printf("%lf\n", array[counter]);
+
+		if (min_array_element > array[counter]) {
+			min_array_element = array[counter];
+		}
+	}
+
+	printf("Found smallest element of the generated array: %lf", min_array_element);
+}
---
+++ my_lib.h
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+//version 0.4
+
+int int_rand_min_max(const int min, const int max);
+int input_int_number();
+int get_int_number_on_positive();
+int get_int_number_on_one_or_zero();
+int get_int_number_than_greater_one();
+double input_double_number();
+double double_rand_min_max(const double min, const double max);
+double get_double_number_than_greater_one();
+double get_double_number_on_positive();
